eddc79993e6692ff56c14ecc76e8c22a82dbc87c
Anurag Makineni <anurag.makineni@gmail.com>
2017-11-12 19:28:02 -0500

    skeleton file for boat dynamics

---
+++ dynamic_model/BoatDynamics.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class BoatDynamics:
+    def __init__(self):
+        pass
+
+    def calculateWrench(self, state):
+        pass
+
+    def calculateDerivatives(self):
+        pass
