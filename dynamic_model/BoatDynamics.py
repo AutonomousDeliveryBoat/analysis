dd1370beaea2132908af6b223733ba70a99d5ffd
Anurag Makineni <anurag.makineni@gmail.com>
2017-11-12 23:14:28 -0500

    update dynamics script

--- dynamic_model/BoatDynamics.py
+++ dynamic_model/BoatDynamics.py
@@ -1,11 +1,12 @@
 import numpy as np
 
+
 class BoatDynamics:
     def __init__(self):
         pass
 
-    def calculateWrench(self, state):
+    def calculate_wrench(self, state, control):
         pass
 
-    def calculateDerivatives(self):
+    def calculate_derivatives(self, state, control):
         pass
